ad8349c4372e5db5f40ae615aeae7719a28cf599
Vinicius Moraes <vmoraes424@gmail.com>
2026-05-12 15:01:35 -0300

    Arruma errinhos

--- src/components/playlist/PlaylistCurrentBlock.tsx
+++ src/components/playlist/PlaylistCurrentBlock.tsx
@@ -61,7 +61,7 @@ export function PlaylistCurrentBlock({
           Tocou
         </p>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+          stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
           className="lucide lucide-list-chevrons-up-down playlist-compact-icon playlist-compact-icon-compact"
           aria-hidden="true">
           <path d="M3 5h8" />
